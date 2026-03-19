c1814a099f067441423932462abc0df476b2eeef
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-19 10:04:55 +0530

    feat: refactor server initialization to include MongoDB connection handling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './db/database.js';
 dotenv.config({
     path: './.env'
 });
 
 
-
 const port = process.env.PORT || 3000;
 
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`🚀 Server running on port http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("❌ MongoDB connection error:", err);
+        process.exit(1);
+    })
 
 
-app.listen(port, () => {
-    console.log(`🚀 Server is running on port http://localhost:${port}`);
-});
