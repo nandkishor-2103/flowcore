d0d90c0db85107f0a18ca279b129c113eff33db3
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-19 15:23:07 +0530

    feat: add JWT authentication methods and temporary token generation to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
     {
@@ -59,6 +61,7 @@ const userSchema = new Schema(
     { timestamps: true },
 );
 
+// ==================Hooks=================
 // Hash password before saving the user document to the database
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
@@ -67,9 +70,54 @@ userSchema.pre("save", async function (next) {
     next();
 });
 
+
+// ==================Methods=================
 // Method to compare the provided password with the hashed password in the database
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
+
+// Method to generate an access token for the user
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+
+// Method to generate a refresh token for the user
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
+
+// Method to generate a temporary token for password reset or email verification
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken =crypto.randomBytes(32).toString("hex");
+
+    const hashedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex");
+
+    const tokenExpiry = Date.now() + (20 * 60 * 1000); // Token valid for 20 minutes
+
+    return {
+        unHashedToken,
+        hashedToken,
+        tokenExpiry,
+    };
+}
 
 export const User = mongoose.model("User", userSchema);
