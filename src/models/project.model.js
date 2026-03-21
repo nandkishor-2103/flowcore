e0167907110a5ba9c4a04554e03212d57f06284c
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-21 11:22:40 +0530

    feat: add models for Project, ProjectNote, ProjectMember, Subtask, and Task with schemas and exports

---
+++ src/models/project.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+
+        description: {
+            type: String,
+            trim: true,
+        },
+
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+const Project = mongoose.model("Project", projectSchema);
+
+export { Project };
